03cbe65eb9e45c4f89f51ed9ff8112c0abc77cff
gauravsahu9668 <sahug6194@gmail.com>
2024-12-22 00:23:19 +0530

    newchnages

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,7 @@ createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/signup" element={<Home/>} />
         <Route path="/testimonial/:id" element={<Testimonial />} />
       </Routes>
     </BrowserRouter>

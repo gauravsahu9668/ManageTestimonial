a961d090b8c5a204bdf146d0c3ec3db4fea9009a
gauravsahu9668 <sahug6194@gmail.com>
2024-12-21 23:49:15 +0530

    addhome

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@
 import './App.css'
 import { Routes,Route } from 'react-router-dom'
 import Testimonial from './Testimonials/Testimonial'
+import Home from './Testimonials/Home'
 function App() {
   return (
     <>
       <Routes>
         <Route path='/testimonial/:id' element={<Testimonial></Testimonial>}></Route>
+        <Route path='/' element={<Home></Home>}></Route>
       </Routes>
     </>
   )

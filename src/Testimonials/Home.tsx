4f21e7110646076946d47acf853778d8905e014c
gauravsahu9668 <sahug6194@gmail.com>
2024-12-22 00:41:05 +0530

    new commit

--- src/Testimonials/Home.tsx
+++ src/Testimonials/Home.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 const Home = () => {
   return (
     <div>
-      <Link to={'/testimonial'}>
+      <Link to={'/testimonial/1'}>
       <div>mja nhi a rha</div>
       </Link>
     </div>
